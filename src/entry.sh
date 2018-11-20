39e770666d8a1e97397c7ce14c43ab90861b48c4
Andre Branchizio <andrejbranch@gmail.com>
2018-11-19 17:54:45 -0700

    set default values for parameters

--- src/entry.sh
+++
@@ -1,3 +0,0 @@
-/usr/local/apache2/bin/apachectl start
-sudo -u postgres pg_ctl -D postgres -l postgres/logfile start
-
--- src/src/Controller/ReportingController.php
+++ src/src/Controller/ReportingController.php
@@ -59,12 +59,18 @@ class ReportingController extends AbstractController
 
     private function extractParams(Request $request)
     {
+        // instead of failing for lack of query params just return last day
+        // worth of data
+        $today = new \DateTime();
+        $yesterday = new \DateTime();
+        $yesterday->modify('-1 day');
+
         return [
-            'reportType' => $request->query->get('report'),
-            'businessId' => $request->query->get('business_id'),
-            'start' => $request->query->get('start'),
-            'end' => $request->query->get('end'),
-            'timeInterval' => $request->query->get('timeInterval'),
+            'reportType' => $request->query->get('report', 'EGS'),
+            'businessId' => $request->query->get('business_id', 'e0b6683d-5efc-4b7a-836d-f3a3fe16ebae'),
+            'start' => $request->query->get('start', $yesterday->format('Y-m-d\TH:i:s.000\Z')),
+            'end' => $request->query->get('end', $today->format('Y-m-d\TH:i:s.000\Z')),
+            'timeInterval' => $request->query->get('timeInterval', 'day'),
             'limit' => $request->query->get('limit', 100),
             'offset' => $request->query->get('offset', 0),
         ];
